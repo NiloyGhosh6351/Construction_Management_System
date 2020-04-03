2412763e3c2185ebbf50b9ee2250616f552fbc4a
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-04-04 03:25:55 +0600

    supplier gui

--- Construction_Management_System/Gui Design/Home.Designer.cs
+++ Construction_Management_System/Gui Design/Home.Designer.cs
@@ -204,6 +204,7 @@
             this.button6.Size = new System.Drawing.Size(204, 87);
             this.button6.TabIndex = 0;
             this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // panel4
             // 
@@ -273,6 +274,7 @@
             this.button4.Size = new System.Drawing.Size(204, 87);
             this.button4.TabIndex = 0;
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // pictureBox4
             // 

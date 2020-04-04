04dca1241687009bf3c53f2286d815fcb2ef45f1
NiloyGhosh6351 <62530599+NiloyGhosh6351@users.noreply.github.com>
2020-04-04 18:09:26 +0600

    update

--- Construction_Management_System/Gui Design/Item.cs
+++ Construction_Management_System/Gui Design/Item.cs
@@ -36,7 +36,7 @@ namespace Construction_Management_System
         private void buttonItemAdd_Click(object sender, EventArgs e)
         {
             string Connectionstring = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ANUP\Documents\Construction_Management_System.mdf;Integrated Security=True;Connect Timeout=30";
-            string sql = string.Format("insert into Item (Item_ID, Item_Name, Item_Price, Item_Catagory, Supplier_Name) Values('{0}','{1}','{2}','{3}','{4}')", textBoxItemId.Text, textBoxItemName.Text,textBoxItemPrice.Text,textBoxCategory.Text,textBoxSupplierName.Text);
+            string sql = string.Format("insert into Item (Item_ID, Item_Name, Item_Price, Item_Catagory, Supplier_Name, Item_Quantity) Values('{0}','{1}','{2}','{3}','{4}','{5}')", textBoxItemId.Text, textBoxItemName.Text,textBoxItemPrice.Text,textBoxCategory.Text,textBoxSupplierName.Text,textBoxItemQuantity.Text);
             SqlConnection con1 = new SqlConnection(Connectionstring);
             SqlCommand sqlcmd = new SqlCommand(sql, con1);
             DataTable dt1 = new DataTable();

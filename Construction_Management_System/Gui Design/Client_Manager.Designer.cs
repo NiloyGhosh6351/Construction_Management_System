6ae5f866ec430d0dab06e873cb10491df72bae24
nayeem99 <62529328+nayeem99@users.noreply.github.com>
2020-04-27 16:15:30 +0600

    manager client

--- Construction_Management_System/Gui Design/Client_Manager.Designer.cs
+++ Construction_Management_System/Gui Design/Client_Manager.Designer.cs
@@ -67,16 +67,18 @@
             this.panelClient.Controls.Add(this.labelClientId);
             this.panelClient.Controls.Add(this.labelClient);
             this.panelClient.Location = new System.Drawing.Point(0, 0);
+            this.panelClient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelClient.Name = "panelClient";
-            this.panelClient.Size = new System.Drawing.Size(211, 521);
+            this.panelClient.Size = new System.Drawing.Size(281, 641);
             this.panelClient.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Construction_Management_System.Properties.Resources.arrow;
-            this.pictureBox1.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(19, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(39, 43);
+            this.pictureBox1.Size = new System.Drawing.Size(52, 53);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
@@ -84,103 +86,103 @@
             // 
             // textBoxClientContact
             // 
-            this.textBoxClientContact.Location = new System.Drawing.Point(30, 355);
-            this.textBoxClientContact.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxClientContact.Location = new System.Drawing.Point(40, 437);
+            this.textBoxClientContact.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxClientContact.Name = "textBoxClientContact";
-            this.textBoxClientContact.Size = new System.Drawing.Size(152, 20);
+            this.textBoxClientContact.Size = new System.Drawing.Size(201, 22);
             this.textBoxClientContact.TabIndex = 20;
+            this.textBoxClientContact.TextChanged += new System.EventHandler(this.textBoxClientContact_TextChanged);
             // 
             // labelClientEmail
             // 
             this.labelClientEmail.AutoSize = true;
             this.labelClientEmail.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelClientEmail.ForeColor = System.Drawing.Color.White;
-            this.labelClientEmail.Location = new System.Drawing.Point(28, 278);
-            this.labelClientEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelClientEmail.Location = new System.Drawing.Point(37, 342);
             this.labelClientEmail.Name = "labelClientEmail";
-            this.labelClientEmail.Size = new System.Drawing.Size(45, 19);
+            this.labelClientEmail.Size = new System.Drawing.Size(54, 23);
             this.labelClientEmail.TabIndex = 21;
             this.labelClientEmail.Text = "Email";
             // 
             // textBoxClientAddress
             // 
-            this.textBoxClientAddress.Location = new System.Drawing.Point(30, 405);
-            this.textBoxClientAddress.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxClientAddress.Location = new System.Drawing.Point(40, 498);
+            this.textBoxClientAddress.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxClientAddress.Multiline = true;
             this.textBoxClientAddress.Name = "textBoxClientAddress";
-            this.textBoxClientAddress.Size = new System.Drawing.Size(152, 56);
+            this.textBoxClientAddress.Size = new System.Drawing.Size(201, 68);
             this.textBoxClientAddress.TabIndex = 16;
+            this.textBoxClientAddress.TextChanged += new System.EventHandler(this.textBoxClientAddress_TextChanged);
             // 
             // labelSupplierAddress
             // 
             this.labelSupplierAddress.AutoSize = true;
             this.labelSupplierAddress.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSupplierAddress.ForeColor = System.Drawing.Color.White;
-            this.labelSupplierAddress.Location = new System.Drawing.Point(28, 384);
-            this.labelSupplierAddress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSupplierAddress.Location = new System.Drawing.Point(37, 473);
             this.labelSupplierAddress.Name = "labelSupplierAddress";
-            this.labelSupplierAddress.Size = new System.Drawing.Size(63, 19);
+            this.labelSupplierAddress.Size = new System.Drawing.Size(74, 23);
             this.labelSupplierAddress.TabIndex = 19;
             this.labelSupplierAddress.Text = "Address";
             // 
             // textBoxClientEmail
             // 
-            this.textBoxClientEmail.Location = new System.Drawing.Point(31, 303);
-            this.textBoxClientEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxClientEmail.Location = new System.Drawing.Point(41, 373);
+            this.textBoxClientEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxClientEmail.Name = "textBoxClientEmail";
-            this.textBoxClientEmail.Size = new System.Drawing.Size(152, 20);
+            this.textBoxClientEmail.Size = new System.Drawing.Size(201, 22);
             this.textBoxClientEmail.TabIndex = 13;
+            this.textBoxClientEmail.TextChanged += new System.EventHandler(this.textBoxClientEmail_TextChanged);
             // 
             // labelClientContact
             // 
             this.labelClientContact.AutoSize = true;
             this.labelClientContact.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelClientContact.ForeColor = System.Drawing.Color.White;
-            this.labelClientContact.Location = new System.Drawing.Point(28, 334);
-            this.labelClientContact.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelClientContact.Location = new System.Drawing.Point(37, 411);
             this.labelClientContact.Name = "labelClientContact";
-            this.labelClientContact.Size = new System.Drawing.Size(60, 19);
+            this.labelClientContact.Size = new System.Drawing.Size(72, 23);
             this.labelClientContact.TabIndex = 17;
             this.labelClientContact.Text = "Contact";
             this.labelClientContact.Click += new System.EventHandler(this.labelClientContact_Click);
             // 
             // textBoxClientName
             // 
-            this.textBoxClientName.Location = new System.Drawing.Point(30, 247);
-            this.textBoxClientName.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxClientName.Location = new System.Drawing.Point(40, 304);
+            this.textBoxClientName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxClientName.Name = "textBoxClientName";
-            this.textBoxClientName.Size = new System.Drawing.Size(152, 20);
+            this.textBoxClientName.Size = new System.Drawing.Size(201, 22);
             this.textBoxClientName.TabIndex = 11;
+            this.textBoxClientName.TextChanged += new System.EventHandler(this.textBoxClientName_TextChanged);
             // 
             // labelClientName
             // 
             this.labelClientName.AutoSize = true;
             this.labelClientName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelClientName.ForeColor = System.Drawing.Color.White;
-            this.labelClientName.Location = new System.Drawing.Point(28, 222);
-            this.labelClientName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelClientName.Location = new System.Drawing.Point(37, 273);
             this.labelClientName.Name = "labelClientName";
-            this.labelClientName.Size = new System.Drawing.Size(91, 19);
+            this.labelClientName.Size = new System.Drawing.Size(109, 23);
             this.labelClientName.TabIndex = 15;
             this.labelClientName.Text = "Client Name";
             // 
             // textBoxClientId
             // 
-            this.textBoxClientId.Location = new System.Drawing.Point(30, 187);
-            this.textBoxClientId.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxClientId.Location = new System.Drawing.Point(40, 230);
+            this.textBoxClientId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxClientId.Name = "textBoxClientId";
-            this.textBoxClientId.Size = new System.Drawing.Size(152, 20);
+            this.textBoxClientId.Size = new System.Drawing.Size(201, 22);
             this.textBoxClientId.TabIndex = 10;
+            this.textBoxClientId.TextChanged += new System.EventHandler(this.textBoxClientId_TextChanged);
             // 
             // labelClientId
             // 
             this.labelClientId.AutoSize = true;
             this.labelClientId.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelClientId.ForeColor = System.Drawing.Color.White;
-            this.labelClientId.Location = new System.Drawing.Point(28, 162);
-            this.labelClientId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelClientId.Location = new System.Drawing.Point(37, 199);
             this.labelClientId.Name = "labelClientId";
-            this.labelClientId.Size = new System.Drawing.Size(65, 19);
+            this.labelClientId.Size = new System.Drawing.Size(80, 23);
             this.labelClientId.TabIndex = 12;
             this.labelClientId.Text = "Client ID";
             // 
@@ -189,9 +191,10 @@
             this.labelClient.AutoSize = true;
             this.labelClient.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelClient.ForeColor = System.Drawing.Color.White;
-            this.labelClient.Location = new System.Drawing.Point(41, 91);
+            this.labelClient.Location = new System.Drawing.Point(55, 112);
+            this.labelClient.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelClient.Name = "labelClient";
-            this.labelClient.Size = new System.Drawing.Size(123, 50);
+            this.labelClient.Size = new System.Drawing.Size(153, 62);
             this.labelClient.TabIndex = 0;
             this.labelClient.Text = "Client";
             // 
@@ -199,9 +202,11 @@
             // 
             this.dataGridViewClient.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewClient.Location = new System.Drawing.Point(237, 35);
+            this.dataGridViewClient.Location = new System.Drawing.Point(316, 43);
+            this.dataGridViewClient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridViewClient.Name = "dataGridViewClient";
-            this.dataGridViewClient.Size = new System.Drawing.Size(509, 382);
+            this.dataGridViewClient.RowHeadersWidth = 51;
+            this.dataGridViewClient.Size = new System.Drawing.Size(679, 470);
             this.dataGridViewClient.TabIndex = 2;
             // 
             // buttonClientClear
@@ -209,24 +214,28 @@
             this.buttonClientClear.BackColor = System.Drawing.Color.Yellow;
             this.buttonClientClear.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonClientClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.buttonClientClear.Location = new System.Drawing.Point(638, 444);
+            this.buttonClientClear.Location = new System.Drawing.Point(851, 546);
+            this.buttonClientClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonClientClear.Name = "buttonClientClear";
-            this.buttonClientClear.Size = new System.Drawing.Size(96, 37);
+            this.buttonClientClear.Size = new System.Drawing.Size(128, 46);
             this.buttonClientClear.TabIndex = 17;
             this.buttonClientClear.Text = "CLEAR";
             this.buttonClientClear.UseVisualStyleBackColor = false;
+            this.buttonClientClear.Click += new System.EventHandler(this.buttonClientClear_Click);
             // 
             // buttonIClientDelete
             // 
             this.buttonIClientDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.buttonIClientDelete.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonIClientDelete.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.buttonIClientDelete.Location = new System.Drawing.Point(512, 444);
+            this.buttonIClientDelete.Location = new System.Drawing.Point(683, 546);
+            this.buttonIClientDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonIClientDelete.Name = "buttonIClientDelete";
-            this.buttonIClientDelete.Size = new System.Drawing.Size(96, 37);
+            this.buttonIClientDelete.Size = new System.Drawing.Size(128, 46);
             this.buttonIClientDelete.TabIndex = 16;
             this.buttonIClientDelete.Text = "DELETE";
             this.buttonIClientDelete.UseVisualStyleBackColor = false;
+            this.buttonIClientDelete.Click += new System.EventHandler(this.buttonIClientDelete_Click);
             // 
             // buttonClientUpdate
             // 
@@ -234,40 +243,46 @@
             this.buttonClientUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.buttonClientUpdate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonClientUpdate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.buttonClientUpdate.Location = new System.Drawing.Point(384, 444);
+            this.buttonClientUpdate.Location = new System.Drawing.Point(512, 546);
+            this.buttonClientUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonClientUpdate.Name = "buttonClientUpdate";
-            this.buttonClientUpdate.Size = new System.Drawing.Size(96, 37);
+            this.buttonClientUpdate.Size = new System.Drawing.Size(128, 46);
             this.buttonClientUpdate.TabIndex = 15;
             this.buttonClientUpdate.Text = "UPDATE";
             this.buttonClientUpdate.UseVisualStyleBackColor = false;
+            this.buttonClientUpdate.Click += new System.EventHandler(this.buttonClientUpdate_Click);
             // 
             // buttonClientAdd
             // 
             this.buttonClientAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.buttonClientAdd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonClientAdd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.buttonClientAdd.Location = new System.Drawing.Point(246, 444);
+            this.buttonClientAdd.Location = new System.Drawing.Point(328, 546);
+            this.buttonClientAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonClientAdd.Name = "buttonClientAdd";
-            this.buttonClientAdd.Size = new System.Drawing.Size(96, 37);
+            this.buttonClientAdd.Size = new System.Drawing.Size(128, 46);
             this.buttonClientAdd.TabIndex = 14;
             this.buttonClientAdd.Text = "ADD";
             this.buttonClientAdd.UseVisualStyleBackColor = false;
+            this.buttonClientAdd.Click += new System.EventHandler(this.buttonClientAdd_Click);
             // 
             // Client_Manager
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(771, 516);
+            this.ClientSize = new System.Drawing.Size(1028, 635);
             this.Controls.Add(this.buttonClientClear);
             this.Controls.Add(this.buttonIClientDelete);
             this.Controls.Add(this.buttonClientUpdate);
             this.Controls.Add(this.buttonClientAdd);
             this.Controls.Add(this.dataGridViewClient);
             this.Controls.Add(this.panelClient);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Client_Manager";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CLIENT";
+            this.Load += new System.EventHandler(this.Client_Manager_Load);
             this.panelClient.ResumeLayout(false);
             this.panelClient.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- Construction_Management_System/Gui Design/Client_Manager.cs
+++ Construction_Management_System/Gui Design/Client_Manager.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Data.SqlClient;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +14,18 @@ namespace Construction_Management_System.Gui_Design
 {
     public partial class Client_Manager : Form
     {
+        string Connectionstring;
         public Client_Manager()
         {
             InitializeComponent();
-        }
+            string currentLocation = Directory.GetCurrentDirectory();
+            string projectDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(currentLocation).FullName).FullName).FullName;
+            Connectionstring = string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={0}\Construction_Management_System.mdf;Integrated Security=True;Connect Timeout=30", projectDir);
+            dataGridViewClient.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewClient.MultiSelect = false;
 
+        }
+        int selectedRow;
         private void labelClientContact_Click(object sender, EventArgs e)
         {
 
@@ -29,5 +38,135 @@ namespace Construction_Management_System.Gui_Design
             f1.Show();
             this.Hide();
         }
+
+        private void Client_Manager_Load(object sender, EventArgs e)
+        {
+            string sql = string.Format("select * " + " from Client");
+            SqlConnection con1 = new SqlConnection(Connectionstring);
+            SqlCommand sqlcmd = new SqlCommand(sql, con1);
+            DataTable dt1 = new DataTable();
+            sqlcmd.Connection.Open();
+            dt1.Load(sqlcmd.ExecuteReader());
+            sqlcmd.Connection.Close();
+            display_dataClient();
+
+        }
+
+
+        private void textBoxClientId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxClientName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxClientEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxClientContact_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxClientAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClientAdd_Click(object sender, EventArgs e)
+        {
+            string sql = string.Format("insert into Client (Client_ID, Client_Name, Client_Contact, Client_Email, Client_Address) Values('{0}','{1}','{2}','{3}','{4}')", textBoxClientId.Text, textBoxClientName.Text, textBoxClientContact.Text, textBoxClientEmail.Text, textBoxClientAddress.Text);
+            SqlConnection con1 = new SqlConnection(Connectionstring);
+            SqlCommand sqlcmd = new SqlCommand(sql, con1);
+            DataTable dt1 = new DataTable();
+            sqlcmd.Connection.Open();
+            sqlcmd.ExecuteNonQuery();
+            MessageBox.Show("Add successfully");
+
+            sqlcmd.Connection.Close();
+            display_dataClient();
+            MessageBox.Show("Item Added Successfully");
+            buttonClientClear_Click(new object(), new EventArgs());
+
+        }
+
+        public void display_dataClient()
+        {
+            string sql = string.Format("select * " + " from Client");
+            SqlConnection con1 = new SqlConnection(Connectionstring);
+            SqlCommand sqlcmd = new SqlCommand(sql, con1);
+            DataTable dt1 = new DataTable();
+            sqlcmd.Connection.Open();
+            sqlcmd.ExecuteNonQuery();
+            SqlDataAdapter data1 = new SqlDataAdapter(sqlcmd);
+            //MessageBox.Show("Add successfully");
+            data1.Fill(dt1);
+            dataGridViewClient.DataSource = dt1;
+            sqlcmd.Connection.Close();
+        }
+
+
+        private void buttonClientUpdate_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow dataGridViewRow = dataGridViewClient.Rows[selectedRow];
+            dataGridViewClient.SelectedCells[0].Value = textBoxClientId.Text;
+            dataGridViewClient.SelectedCells[1].Value = textBoxClientName.Text;
+            dataGridViewClient.SelectedCells[2].Value = textBoxClientContact.Text;
+            dataGridViewClient.SelectedCells[3].Value = textBoxClientEmail.Text;
+            dataGridViewClient.SelectedCells[4].Value = textBoxClientAddress.Text;
+            string sql = string.Format("update Client set Client_ID={0}, Client_Name='{1}', Client_Email='{2}', Client_Contact='{3}', Client_Address='{4}'  where  Client_ID={5} ", textBoxClientId.Text, textBoxClientName.Text, textBoxClientEmail.Text, textBoxClientContact.Text, textBoxClientAddress.Text, textBoxClientId.Text);
+            SqlConnection con1 = new SqlConnection(Connectionstring);
+            SqlCommand sqlcmd = new SqlCommand(sql, con1);
+            DataTable dt1 = new DataTable();
+            sqlcmd.Connection.Open();
+            sqlcmd.ExecuteNonQuery();
+            MessageBox.Show("Update Successfully");
+
+            sqlcmd.Connection.Close();
+            display_dataClient();
+
+
+        }
+
+        private void buttonIClientDelete_Click(object sender, EventArgs e)
+        {
+            string sql = string.Format("delete " + " from Client where Client_ID={0}", textBoxClientId.Text);
+            SqlConnection con1 = new SqlConnection(Connectionstring);
+            SqlCommand sqlcmd = new SqlCommand(sql, con1);
+            DataTable dt1 = new DataTable();
+            sqlcmd.Connection.Open();
+            sqlcmd.ExecuteNonQuery();
+            sqlcmd.Connection.Close();
+            display_dataClient();
+            MessageBox.Show("Delete Successfully");
+            buttonClientClear_Click(new object(), new EventArgs());
+
+        }
+
+        private void buttonClientClear_Click(object sender, EventArgs e)
+        {
+            textBoxClientId.Clear();
+            textBoxClientName.Clear();
+            textBoxClientContact.Clear();
+            textBoxClientEmail.Clear();
+            textBoxClientAddress.Clear();
+
+        }
+        private void dataGridViewClient_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+            DataGridViewRow row = dataGridViewClient.Rows[selectedRow];
+            textBoxClientId.Text = row.Cells[0].Value.ToString();
+            textBoxClientName.Text = row.Cells[1].Value.ToString();
+            textBoxClientContact.Text = row.Cells[2].Value.ToString();
+            textBoxClientEmail.Text = row.Cells[3].Value.ToString();
+            textBoxClientAddress.Text = row.Cells[4].Value.ToString();
+        }
+
     }
 }
